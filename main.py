67177e5e86bcb7b480c692adb5f614a06bd33ab3
Kai Chen <kaichenusesthis@gmail.com>
2017-06-24 12:52:09 -0700

    Script now finds user's name and logs it to console.

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 import time
 
-driver = webdriver.Firefox()
-driver.get("http://webcat.vsb.bc.ca/ipac20/ipac.jsp?session=&profile=ls&auth=false&submenu=subtab13&date=")
-
 studentID = 811821
-while True:
+MASTER_URL = "http://webcat.vsb.bc.ca/ipac20/ipac.jsp?session=&profile=ls&auth=false&submenu=subtab13&date="
+driver = webdriver.Firefox()
+driver.get(MASTER_URL)
+
+def waitForPageToLoad():
+	time.sleep(1)
+
+def logout():
+	waitForPageToLoad()
+	# globalAnchor is the logout button
+	logout = driver.find_element_by_class_name("globalAnchor")
+	# click it
+	logout.send_keys(Keys.RETURN)
+
+	driver.get(MASTER_URL)
+
+def login():
+	# make sure we are on correcr page
 	assert "VSB webcat" in driver.title
+	# find login field
 	elem = driver.find_element_by_name("sec1")
 	elem.clear()
 	elem.send_keys(studentID)
+	print(studentID)
 	elem.send_keys(Keys.RETURN)
 
-	time.sleep(5)
-	logout = driver.find_element_by_class_name("globalAnchor")
-	logout.send_keys(Keys.RETURN)
+def getLoggedInName():
+	waitForPageToLoad()
+	name = driver.find_element_by_xpath("//td[@title='Name']")
+	loggedInName = name.get_attribute('innerHTML')
+	print(loggedInName)
 
-	driver.get("http://webcat.vsb.bc.ca/ipac20/ipac.jsp?session=&profile=ls&auth=false&submenu=subtab13&date=")
+
+login()
+while True:
+	try: 
+		getLoggedInName()
+		logout()
+
+	except: 
+		logout()
 
 	studentID += 1
+	login()
+
+
+
+
 
