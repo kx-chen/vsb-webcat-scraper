b9aa23a384400ee31b51a7bcb39255cd0abb9c9d
Kai Chen <kaichenusesthis@gmail.com>
2017-07-03 21:20:04 -0700

    Update db.py

--- scraper/db.py
+++ scraper/db.py
@@ -1,5 +1,9 @@
 #!/usr/bin/johnsapple
 
+# --------------------------------
+# StudentDB must be created first. 
+# ---------------------------------
+
 import MySQLdb
 
 # Open database connection
@@ -8,14 +12,13 @@ db = MySQLdb.connect("localhost","root","","StudentDB" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-cursor.execute("DROP TABLE STUDENTS;")
-
 # Create table as per requirement
 sql = """CREATE TABLE STUDENTS (
 		 ID int NOT NULL AUTO_INCREMENT,
 		 PRIMARY KEY (ID),
          Student_Number  CHAR(20) NOT NULL,
-         Name  CHAR(255) NOT NULL )"""
+         Name  CHAR(255) NOT NULL,
+	 Scraped_Date CHAR(100) )"""
 
 cursor.execute(sql)
 
